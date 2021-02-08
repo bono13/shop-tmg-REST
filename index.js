2c6f75e613cc414b286443c0a8f057e6e18249a8
bono13 <cornejo.dev@gmail.com>
2021-02-09 02:04:35 +0300

    init multer implmentation

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+const path = require('path');
+
+//IMPORTS
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const passport = require('passport');
+const multer = require('multer');
 
 //MODULES
 const productRouter = require('./src/routers/product');
@@ -14,7 +18,7 @@ const port = process.env.PORT;
 //PASSPORT CONFIG
 require('./src/utils/passport-config');
 
-// Middleware
+// MIDDLEWARE
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(passport.initialize());
@@ -23,6 +27,41 @@ app.use(bodyParser.urlencoded({ extended: false }));
 //ROUTES
 app.use(userRouter);
 app.use(productRouter);
+
+/**Multer */
+app.use('/uploads', express.static(path.join(__dirname, './src/uploads')));
+
+const storage = multer.diskStorage({
+	destination: (req, file, cb) => {
+		cb(null, './src/uploads');
+	},
+	filename: (req, file, cb) => {
+		console.log(file);
+		cb(null, Date.now() + path.extname(file.originalname));
+	},
+});
+const fileFilter = (req, file, cb) => {
+	if (file.mimetype == 'image/jpeg' || file.mimetype == 'image/png') {
+		cb(null, true);
+	} else {
+		cb(null, false);
+	}
+};
+
+const upload = multer({ storage: storage, fileFilter: fileFilter });
+
+//Upload route
+app.post('/upload', upload.single('image'), (req, res, next) => {
+	try {
+		return res.status(201).json({
+			message: 'File uploaded successfully',
+		});
+	} catch (error) {
+		console.error(error);
+	}
+});
+
+/**Multer */
 
 //DB connection
 mongoose.connect(
